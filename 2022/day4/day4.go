10b48cd475d53f75cd320c25892fc223a525daa8
Victor Kaugesaar <kaugesaar@gmail.com>
2022-12-06 00:04:21 +0100

    refactor 🔨

--- 2022/day4/day4.go
+++ 2022/day4/day4.go
@@ -3,40 +3,26 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"strconv"
 	"strings"
 )
 
 //go:embed day4.txt
 var fileInput string
 
-func fullyContains(a []int, b []int) bool {
-	return (a[0] <= b[0] && a[1] >= b[1]) || (b[0] <= a[0] && b[1] >= a[1])
+func fullyContains(a int, b int, c int, d int) bool {
+	return (a <= c && b >= d) || (c <= a && d >= b)
 }
 
-func overlaps(a []int, b []int) bool {
-	return (a[0] <= b[0] && b[0] <= a[1]) || (b[0] <= a[0] && a[0] <= b[1])
+func overlaps(a int, b int, c int, d int) bool {
+	return !(b < c || d < a)
 }
 
-func parseSection(assignments string) []int {
-	sections := strings.Split(assignments, "-")
-	low, _ := strconv.Atoi(sections[0])
-	high, _ := strconv.Atoi(sections[1])
-	return []int{low, high}
-}
-
-func parsePair(pairs string) ([]int, []int) {
-	assignments := strings.Split(pairs, ",")
-	a := parseSection(assignments[0])
-	b := parseSection(assignments[1])
-	return a, b
-}
-
-func parser(lines []string, checker func([]int, []int) bool) int {
+func parser(lines []string, checker func(int, int, int, int) bool) int {
 	var count int
-	for _, pairs := range lines {
-		a, b := parsePair(pairs)
-		if checker(a, b) {
+	for _, line := range lines {
+		var a, b, c, d int
+		fmt.Sscanf(line, "%d-%d,%d-%d", &a, &b, &c, &d)
+		if checker(a, b, c, d) {
 			count++
 		}
 	}
